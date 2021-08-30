64af85f957d366673d6f28ae761f6dae2a320621
mmevy <mervemetinogluu@gmail.com>
2021-08-30 20:24:27 +0300

    added web server

---
+++ custom-web-server/index.js
@@ -0,0 +1,27 @@
+var http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>WELCOME TO INDEX PAGE</h2>");
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>WELCOME TO ABOUT PAGE</h2>");
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>WELCOME TO CONTACT PAGE</h2>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h2>404 PAGE NOT FOUND</h2>");
+  }
+
+  res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server is starting on port ${port} `);
+});
